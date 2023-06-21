dddd08570b6577550b2c616e966cf262194cc7a1
Tomer <tomermi123@gmail.com>
2023-06-21 08:06:21 +0300

    app.js without comments

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const express = require('express');   //Imports the Express framework. allows us to create and configure the server.
-const path = require('path'); //Provides utilities for working with file and directory paths.
-const bodyParser = require('body-parser');  //Parses incoming request bodies. makes data available in 'req.body'
-const cors = require('cors'); //Enables Cross-Origin Resources Sharing. allows requests from other domains.
-const mongoose = require('mongoose'); //Connces to a MongoDB database using Mongoose library.
-const indexRouter=require('./routes');  //imports index router file. cotains defined routes for server.
-// const newLocal = require('custom-env')  //Imports custom-env package for configuring environment variables.
-// newLocal.env(process.env.NODE_ENV,'./config');  //Sets the environment variables
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const indexRouter = require('./routes');
 
 const PORT = 4000;
-const CONNECTION_STRING = 'mongodb://127.0.0.1/StoicReads'
-mongoose.connect(process.env.CONNECTION_STRING,{useNewUrlParser:true,useUnifiedTopology:true}); //Connects to the MongoDB database using the provided connection string.
-const app = express();  // Creates an instance of the Express application.
-app.use(cors());  // Applies the CORS middleware to allow cross-origin requests.
-app.use(bodyParser.urlencoded({extended:true}));  // Configures the server to parse URL-encoded request bodies.
-app.use(express.json());  //Configures the server to parse JSON request bodies.
-app.use(express.static(path.join(__dirname, './client')));  //Serves static files from specified dir, like HTML, CSS and JS files.
-app.use('/api',indexRouter);  //Mounts the index router at the '/api' endpoint.
+const CONNECTION_STRING = 'mongodb://127.0.0.1/StoicReads';
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.listen(PORT, ()=>console.log('Server is online.')); //Starts the server and listens for incoming requests on the specified port.
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './client')));
+app.use('/api', indexRouter);
 
+app.listen(PORT, () => console.log('Server is online.'));
