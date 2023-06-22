654f49f01530facc81a7bfdfb00a06c7db095bef
Ori <ori100no@gmail.com>
2023-06-22 14:39:59 +0300

    changed the homepage

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 // This file will serve as the entry point for my server.
 const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 
 //Middleware
-app.use(express.json()); // Parse JSON requests
-app.use(express.urlencoded({extended: false})); // Parse URL-encoded requests
-// Set up additional middleware as needed, such as handling CORS or serving static files
-
-// Routes
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './client')));
 app.get('/', (req,res)=>{
-    res.send("Setting up Server using Express.js");
+   res.sendFile(__dirname + '/client/index.html')
 });
+app.get('/login-page', (req,res)=>{
+    res.sendFile(__dirname + '/client/login.html')
+})
 
-// Server
 const port = process.env.PORT || 4000; 
 app.listen(port, ()=>{
     console.log('Server is running on port:' + port);
