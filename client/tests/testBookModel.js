88d39d8db95642184bea3372686dfff836f0584a
Ori <ori100no@gmail.com>
2023-06-23 13:51:02 +0300

    tested db books fetch

---
+++ client/tests/testBookModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    bookID: {
+        type: Number,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    yearOfPublishment: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Book', BookSchema);
