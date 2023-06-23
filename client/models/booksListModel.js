621202e0162987740725a8ace5947ced80ef23b5
Ori <ori100no@gmail.com>
2023-06-23 14:15:05 +0300

    tested db books fetch

--- client/models/booksListModel.js
+++ client/models/booksListModel.js
@@ -1,17 +1,28 @@
-const baseUrl = 'http://localhost:3000';
-let books = [];
-const container = $('#books-list-container');
+
 function getBooks() {
     $.ajax({
         url: 'http://localhost:3000/books',
         type: 'GET',
         success: function(response) {
             $('h1').text('All Books');
-            books = response; // Assuming the response is an array of book objects
+            var books = response; // Assuming the response is an array of book objects
+            var container = $('#books-list-container');
+
             container.empty();
+
             books.forEach(function(book) {
-                var bookElement = $('<div class="flex-center ">');
-                bookElement.text(book.title);
+                var bookElement = $('<div class="card mb-3">');
+                var cardBody = $('<div class="card-body">');
+                var titleElement = $('<h5 class="card-title">').text(book.title);
+                var authorElement = $('<p class="card-text">').text('Author: ' + book.author);
+                var descriptionElement = $('<p class="card-text">').text('Description: ' + book.description);
+                var genreElement = $('<p class="card-text">').text('Genre: ' + book.genre);
+                var yearElement = $('<p class="card-text">').text('Year of Publishment: ' + book.yearOfPublishment);
+                var priceElement = $('<p class="card-text">').text('Price: ' + book.price);
+                var quantityElement = $('<p class="card-text">').text('Quantity: ' + book.quantity);
+
+                cardBody.append(titleElement, authorElement, descriptionElement, genreElement, yearElement, priceElement, quantityElement);
+                bookElement.append(cardBody);
                 container.append(bookElement);
             });
         },
@@ -20,4 +31,6 @@ function getBooks() {
         }
     });
 }
-$('#all-books').click(getBooks);
+$(document).ready(function() {
+    getBooks();
+});
