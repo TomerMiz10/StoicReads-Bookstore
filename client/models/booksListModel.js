4731e415ceb530d5e95b8f8dedb99a3f1f0d5828
Ori <ori100no@gmail.com>
2023-06-23 13:51:02 +0300

    tested db books fetch

---
+++ client/models/booksListModel.js
@@ -0,0 +1,23 @@
+const baseUrl = 'http://localhost:3000';
+let books = [];
+const container = $('#books-list-container');
+function getBooks() {
+    $.ajax({
+        url: 'http://localhost:3000/books',
+        type: 'GET',
+        success: function(response) {
+            // Handle the response here
+            books = response; // Assuming the response is an array of book objects
+            container.empty();
+            books.forEach(function(book) {
+                var bookElement = $('<div>');
+                bookElement.text(book.title);
+                container.append(bookElement);
+            });
+        },
+        error: function(xhr, status, error) {
+            console.log('Failed to retrieve books:', error);
+        }
+    });
+}
+$('#all-books').click(getBooks);
