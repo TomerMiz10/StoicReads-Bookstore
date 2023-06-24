77e25cd74391cc581861f01cb4251dc52db690f4
Ori <ori100no@gmail.com>
2023-06-24 13:16:39 +0300

    added order page

--- client/models/orderPageModel.js
+++ client/models/orderPageModel.js
@@ -1,23 +1,30 @@
 const urlParams = new URLSearchParams(window.location.search);
 const bookId = urlParams.get('id');
+const baseUrl = 'http://localhost:3000';
 
 const onLoaded = () => {
-    // const bookTitle = document.getElementById('bookTitle');
-    // const bookAuthor = document.getElementById('bookAuthor');
-    // const bookGenre = document.getElementById('bookGenre');
-    // const bookPrice = document.getElementById('bookPrice');
-    // const bookDescription = document.getElementById('bookDescription');
-    // const bookImage = document.getElementById('bookImage');
-    // fetch(`http://localhost:3000/books/${bookId}`)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         bookTitle.innerHTML = data.title;
-    //         bookAuthor.innerHTML = data.author;
-    //         bookGenre.innerHTML = data.genre;
-    //         bookPrice.innerHTML = data.price;
-    //         bookDescription.innerHTML = data.description;
-    //         bookImage.src = data.image;
-    //     });
-    console.log(bookId);
+    const bookTitle = document.getElementById('bookTitle');
+    const bookAuthor = document.getElementById('bookAuthor');
+    const bookGenre = document.getElementById('bookGenre');
+    const bookPrice = document.getElementById('bookPrice');
+    const bookDescription = document.getElementById('bookDescription');
+    const bookImage = document.getElementById('bookImage');
+    $.ajax({
+        url: baseUrl+'/books/id/' + bookId,
+        type: 'GET',
+        success: function(response) {
+            const book = response[0];
+            bookTitle.innerHTML = book.title;
+            bookAuthor.innerHTML = "<b>By: </b>"+book.author;
+            bookGenre.innerHTML ="<b>Genre: </b>"+ book.genre;
+            bookPrice.innerHTML ="<b>Price: </b>"+ book.price+"$" ;
+            bookDescription.innerHTML = "<b>Description: </b>"+book.description;
+            bookImage.src = book.image;
+            console.log(book.description);
+        },
+        error: function(xhr, status, error) {
+            console.log('Failed to retrieve book:', error);
+        }
+    });
 }
 window.addEventListener('load', onLoaded);
