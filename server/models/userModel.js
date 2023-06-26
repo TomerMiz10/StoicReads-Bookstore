3c512ec94a6ff8ecad2c5ecbcf012f25d94ff48b
Tomer <tomermi123@gmail.com>
2023-06-26 21:24:16 +0300

    created schema for userModel.js ; adding .env file

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,17 @@
 // represents a customer, with properties like name, email, address, etc.
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+    userID: {type: Number, required: true, unique: true},
+    userName: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, unique: true},
+    fullName: {type: String},
+    address: {type: String},
+    isAdmin: {type: Boolean, required: true},
+    currentShoppingCart: {},
+    orderHistory: {}
+});
+
+module.exports = mongoose.model('User', User);
