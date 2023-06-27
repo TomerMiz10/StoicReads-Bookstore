141aa188a958d2dd6dc5c9ddf67e72586eb410d2
Tomer <tomermi123@gmail.com>
2023-06-27 11:25:51 +0300

    update server.js, and index.html

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 // Routing
-app.use("/", require("./routes/bookRoute/getAllBooks"));
+app.use("/", require("./routes/bookRoute"));
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
     .then(() => {
