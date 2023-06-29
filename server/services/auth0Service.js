a6f3e54d3bcd1c5cda64ca4b5a96aa77c912f8ba
Nitzan <nitzan16001@gmail.com>
2023-06-30 00:08:44 +0300

    * Auth0 Connection

---
+++ server/services/auth0Service.js
@@ -0,0 +1,12 @@
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_CLIENT_SECRET,
+    baseURL: process.env.AUTH0_BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+};
+
+module.exports = {
+    config,
+};
