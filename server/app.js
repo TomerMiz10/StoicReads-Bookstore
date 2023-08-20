18a6505a4ca5e448210902f39ae249fe4819951b
Nitzan <nitzan16001@gmail.com>
2023-08-20 23:29:55 +0300

    * Admin Rendering should also call Google Books API now

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ const app = express();
 const env = require("custom-env").env();
 const cookieParser = require('cookie-parser');
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:63342',
+    credentials: true
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
