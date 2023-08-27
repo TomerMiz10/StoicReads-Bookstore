6dfbab9e4fc5fa9bf38b327874f76f829f990a2f
Tomer <tomermi123@gmail.com>
2023-08-27 14:13:50 +0300

    fixing bad code

--- client/models/showNavbar.js
+++ client/models/showNavbar.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', async function() {
     const navbarContainer = document.getElementById('navbar-container');
     const xhr =await new XMLHttpRequest();
@@ -10,32 +9,3 @@ document.addEventListener('DOMContentLoaded', async function() {
     await xhr.open('GET', 'navbar.html', true);
     xhr.send();
 });
-
-function updateCartCount(userId) {
-    const cartItemCountElement = document.getElementById('cart-item-count');
-    $.ajax({
-        url: baseUrl + '/cart/getCart/' + userId,
-        type: 'GET',
-        success: function(cartItems) {
-            cartItemCountElement.textContent = cartItems.length;
-        },
-        error: function(error){
-            console.error('Error fetching cart. Please try again.', error);
-        }
-    });
-}
-
-window.onload = async () => {
-    const response = await fetch(baseUrl + '/auth/status', {
-        method: 'GET',
-        credentials: 'include'
-    });
-    const data = await response.json();
-    if (data.status) {
-        updateCartCount(data.userId);
-    }
-    else {
-        alert('Please login to view your cart')
-        window.location.href = 'login.html';
-    }
-}
