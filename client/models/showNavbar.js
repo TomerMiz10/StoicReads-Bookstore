43d9506b1c05e0964099a2b225221f802dcbf8c7
Hadar <121627693+SamalArba@users.noreply.github.com>
2023-08-29 00:40:35 +0300

    updated index, navbar, html and styles.

--- client/models/showNavbar.js
+++ client/models/showNavbar.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     xhr.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             navbarContainer.innerHTML = this.responseText;
+            setNavbarState();
         }
     };
     await xhr.open('GET', 'navbar.html', true);
@@ -13,3 +14,38 @@ document.addEventListener('DOMContentLoaded', async function() {
 
 
 
+const setNavbarState = async () => {
+    try {
+        const response = await fetch(baseUrl + '/auth/status', {
+            method: 'GET',
+            credentials: 'include'
+        });
+        const data = await response.json();
+
+        // Default state: Not logged in
+        document.getElementById("signupNav").style.display = "block";
+        document.getElementById("loginNav").style.display = "block";
+        document.getElementById("adminNav").style.display = "none";
+        document.getElementById("logoutNav").style.display = "none";
+
+        if (data.status) {
+            // User is logged in
+            document.getElementById("signupNav").style.display = "none";
+            document.getElementById("loginNav").style.display = "none";
+            document.getElementById("logoutNav").style.display = "block";
+
+            welcomeMessage.text('Welcome back '+data.user.userName+ '! Browse books from our collection.');
+
+            if (data.user.isAdmin) {
+                // User is also an admin
+                document.getElementById("adminNav").style.display = "block";
+            }
+        } else {
+            welcomeMessage.text('Welcome to Stoic Reads book store! Browse books from our collections! Sign up or login to make a purchase.');
+        }
+    } catch (error) {
+        console.error('Error fetching user status:', error);
+    }
+};
+
+document.addEventListener('DOMContentLoaded', setNavbarState);
