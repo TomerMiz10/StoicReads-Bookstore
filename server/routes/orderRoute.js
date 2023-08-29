f39505e2d6d07164a34034e18a7f593f0e6f8a18
Tomer <tomermi123@gmail.com>
2023-08-29 11:47:43 +0300

    fixed order routing

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const orderRoute = express.Router();
+const orderController = require("../controllers/orderController");
 
-
-const {
-    createOrder,
-    deleteOrder
-} = require("../controllers/orderController");
-
-orderRoute.post("/createOrder", createOrder);
-orderRoute.post("/deleteOrder", deleteOrder);
+orderRoute.post("/createOrder", orderController.createOrder);
+orderRoute.post("/deleteOrder/:userId", orderController.deleteOrder);
+orderRoute.get("/getUserById/:userId", orderController.getUserById);
+orderRoute.get("/handlePurchase/", orderController.handlePurchase);
 
 module.exports = orderRoute;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,13 +2,13 @@ const express = require('express');
 const userRoute = express.Router();
 
 const {
-    getUserByID,
+    getUserById,
     getAllUsers,
     getCartByUserId,
     getLastOrderByUserId
 } = require("../controllers/userController");
 
-userRoute.get("/userId/:userID",getUserByID);
+userRoute.get("/userId/:userID",getUserById);
 userRoute.get("/getAllUsers", getAllUsers);
 userRoute.get("/getCartByUserId/:userID", getCartByUserId);
 userRoute.get("/getLastOrderByUserId/:userID", getLastOrderByUserId);
